b6b998ee6e86801ba8523de3a254278361639752
Jefferson <jeffersonxrhudson@gmail.com>
2018-09-22 00:03:10 -0300

    erro na funcao de print

--- Tree.hpp
+++ Tree.hpp
@@ -10,6 +10,13 @@ struct no
 	no<T> *filho_esquerdo;
 	no<T> *irmao_direito;
 	
+	no(T valor)
+	{
+		this->valor = valor;
+		this->pai = nullptr;
+		this->irmao_direito = nullptr;
+		this->filho_esquerdo = nullptr;
+	}
 	no(T valor, no<T> *pai)
 	{
 		this->valor = valor;
@@ -25,21 +32,35 @@ struct no
 		}
 		else
 		{
-			irmao_direito.addirmao(irmao);
+			irmao_direito->addirmao(irmao);
 		}
 	}
 	void addfilho(no<T> *filho)
 	{
+
 		if (filho_esquerdo == nullptr)
 		{
 			filho_esquerdo = filho;
 		}
 		else
 		{
-			filho_esquerdo.addirmao(filho);
+			filho_esquerdo->addirmao(filho);
+		}
+	}
+	void printno()
+	{
+		cout << valor << " ";
+		if (filho_esquerdo != nullptr)
+		{
+			filho_esquerdo->printno();
+		}
+		if (irmao_direito != nullptr)
+		{
+			irmao_direito->printno();
 		}
 	}		
 };
+
 template <typename T>
 class Tree
 {
@@ -54,5 +75,28 @@ class Tree
 		arvore.push_back(no<T>(valor, pai));
 		no<T> *temp = arvore[arvore.size()-1];
 		pai.addfilho(temp); 
+	}
+	void add(const T valor, int pai = 0)
+	{
+		if (pai == 0)
+		{
+			arvore.push_back(no<T>(valor));
+		}
+		else
+		{
+			arvore.push_back(no<T>(valor, &arvore[pai-1]));
+			no<T> *temp = &arvore[arvore.size()-1];
+			arvore[pai-1].addfilho(temp);
+		}
+	}
+	void print()
+	{
+		for (auto i: arvore)
+		{
+			if (i.pai == nullptr)
+			{
+				i.printno();
+			}
+		}
 	}
 };
--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main()
 {
 	Tree<int> arvore;
-
+	arvore.add(3, 0);
+	arvore.add(4, 1);
+	arvore.add(1, 2);
+	arvore.add(2, 1);
+	arvore.print();
 	return 0;
 }
