ef157df7c15b6e96e8a91eed1a03db702a1cb4b3
Jefferson Silva <34176051+Jefferson023@users.noreply.github.com>
2018-09-22 15:48:18 -0300

    desbugado um pouco

--- Tree.hpp
+++ Tree.hpp
@@ -3,99 +3,89 @@
 using namespace std;
 
 template <typename T>
-struct no
-{
-	T valor;
-	no<T> *pai;
-	no<T> *filho_esquerdo;
-	no<T> *irmao_direito;
-	
-	no(T valor)
-	{
-		this->valor = valor;
-		this->pai = nullptr;
-		this->irmao_direito = nullptr;
-		this->filho_esquerdo = nullptr;
-	}
-	no(T valor, no<T> *pai)
-	{
-		this->valor = valor;
-		this->pai = pai;
-		filho_esquerdo = nullptr;
-		irmao_direito = nullptr;
-	}
-	void addirmao(no<T> *irmao)
-	{
-		if (irmao_direito == nullptr)
-		{
-			irmao_direito = irmao;
-		}
-		else
-		{
-			irmao_direito->addirmao(irmao);
-		}
-	}
-	void addfilho(no<T> *filho)
-	{
-
-		if (filho_esquerdo == nullptr)
-		{
-			filho_esquerdo = filho;
-		}
-		else
-		{
-			filho_esquerdo->addirmao(filho);
-		}
-	}
-	void printno()
-	{
-		cout << valor << " ";
-		if (filho_esquerdo != nullptr)
-		{
-			filho_esquerdo->printno();
-		}
-		if (irmao_direito != nullptr)
-		{
-			irmao_direito->printno();
-		}
-	}		
-};
-
-template <typename T>
 class Tree
 {
 	private:
-	vector< no <T> > arvore;
-	
+	struct no
+	{
+		public:
+		T valor;
+		no *pai;
+		no *filho_esquerdo;
+		no *irmao_direito;
+		
+		no(T valor)
+		{
+			this->valor = valor;
+			pai = nullptr;
+			irmao_direito = nullptr;
+			filho_esquerdo = nullptr;
+		}
+
+		void addirmao(no *irmao)
+		{
+			if (irmao_direito == nullptr)
+			{
+				irmao_direito = irmao;
+			}
+			else
+			{
+				irmao_direito->addirmao(irmao);
+			}
+		}
+		void addfilho(no *filho)
+		{
+
+			if (filho_esquerdo == nullptr)
+			{
+				filho_esquerdo = filho;
+			}
+			else
+			{
+				filho_esquerdo->addirmao(filho);
+			}
+		}
+		
+		void print()
+		{
+			cout << " " << valor;
+			if (filho_esquerdo != nullptr)
+			{
+				filho_esquerdo->print();
+			}
+			if (irmao_direito != nullptr)
+			{
+				irmao_direito->print();
+			}
+		}
+	};
+	vector<no*> arvore;
 	public:
 	Tree() = default;
 	
-	void add(const T valor, no<T> *pai = NULL)
-	{
-		arvore.push_back(no<T>(valor, pai));
-		no<T> *temp = arvore[arvore.size()-1];
-		pai.addfilho(temp); 
-	}
 	void add(const T valor, int pai = 0)
 	{
 		if (pai == 0)
 		{
-			arvore.push_back(no<T>(valor));
+			no *novo = new no(valor);
+			arvore.push_back(novo);
 		}
 		else
 		{
-			arvore.push_back(no<T>(valor, &arvore[pai-1]));
-			no<T> *temp = &arvore[arvore.size()-1];
-			arvore[pai-1].addfilho(temp);
+			no *novo = new no(valor);
+			novo->pai = arvore[pai-1];
+			arvore.push_back(novo);
+			arvore[pai-1]->addfilho(novo);
 		}
 	}
+
 	void print()
 	{
-		for (auto i: arvore)
+		for (int i = 0; i < arvore.size(); i++)
 		{
-			if (i.pai == nullptr)
+			if (arvore[i]->pai == nullptr)
 			{
-				i.printno();
+				arvore[i]->print();
 			}
 		}
 	}
--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main()
 	Tree<int> arvore;
 	arvore.add(3, 0);
 	arvore.add(4, 1);
-	arvore.add(1, 2);
 	arvore.add(2, 1);
+	arvore.add(1, 2);
 	arvore.print();
 	return 0;
 }
