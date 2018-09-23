2eb6feb3bc3a388147b08c979ce270ec67b454a1
Jefferson Silva <34176051+Jefferson023@users.noreply.github.com>
2018-09-23 01:07:27 -0300

    tudo pronto, eu acho

--- Tree.hpp
+++ Tree.hpp
@@ -35,6 +35,15 @@ class Tree
 			}
 		}
 
+		void addpai(no *pai)
+		{
+			this->pai = pai;
+			if (irmao_direito)
+			{
+				irmao_direito->addpai(pai);
+			}
+		}
+
 		void addfilho(no *filho)
 		{
 
@@ -54,17 +63,43 @@ class Tree
 			{
 				return true;
 			}
+
 			if (filho_esquerdo && irmao_direito)
 			{
 				return filho_esquerdo->busca_profundidade(chave) || irmao_direito->busca_profundidade(chave);
 			}
+
 			if (filho_esquerdo)
 			{
 				return filho_esquerdo->busca_profundidade(chave);
 			}
+
 			if (irmao_direito)
 			{
 				return irmao_direito->busca_profundidade(chave);
+			}
+			return false;
+		}
+
+		bool busca_largura(const T chave)
+		{
+			if (chave == valor)
+			{
+				return true;
+			}
+			if (irmao_direito == nullptr && filho_esquerdo)
+			{
+				return filho_esquerdo->busca_largura();
+			}
+
+			if (filho_esquerdo == nullptr && irmao_direito)
+			{
+				return irmao_direito->busca_largura();
+			}
+
+			if (irmao_direito && filho_esquerdo)
+			{
+				return irmao_direito->busca_largura() || filho_esquerdo->busca_largura();
 			}
 			return false;
 		}
@@ -80,6 +115,16 @@ class Tree
 			{
 				irmao_direito->print();
 			}
+		}
+
+		bool isroot()
+		{
+			return pai == nullptr;
+		}
+
+		bool isleaf()
+		{
+			return filho_esquerdo == nullptr;
 		}
 	};
 	vector<no*> arvore;
@@ -100,6 +145,82 @@ class Tree
 			novo->pai = arvore[pai-1];
 			arvore.push_back(novo);
 			arvore[pai-1]->addfilho(novo);
+		}
+	}
+
+	void remove(const int posicao)
+	{
+		if (posicao > int(arvore.size()))
+		{
+			return;
+		}
+
+		// caso seja raiz
+		if (arvore[posicao-1]->isroot())
+		{
+			if (!arvore[posicao-1]->filho_esquerdo)
+			{
+				arvore.erase(arvore.begin() + posicao-1);
+			}
+			else
+			{
+				no * temp = arvore[posicao-1]->filho_esquerdo;
+				arvore.erase(arvore.begin() + posicao-1);
+				temp->pai = nullptr;
+				if (temp->irmao_direito)
+				{
+					temp->irmao_direito->addpai(temp);
+					temp->addfilho(temp->irmao_direito);
+					temp->irmao_direito = nullptr;
+				}
+			}
+			return;
+		}
+
+		//caso seja folha
+		if (arvore[posicao-1]->isleaf())
+		{
+			if (arvore[posicao-1]->pai->filho_esquerdo == arvore[posicao-1])
+			{
+				arvore[posicao-1]->pai->filho_esquerdo = arvore[posicao-1]->irmao_direito;
+				arvore.erase(arvore.begin() + posicao-1);
+			}else
+			{
+				no *temp = arvore[posicao-1]->pai->filho_esquerdo;
+				while (temp->irmao_direito != arvore[posicao-1])
+				{
+					temp = temp->irmao_direito;
+				}
+				temp->irmao_direito = arvore[posicao-1]->irmao_direito;
+				arvore.erase(arvore.begin() + posicao-1); 
+			}
+
+			return;
+		}
+
+		//caso seja no interno
+		if (arvore[posicao-1] == arvore[posicao-1]->pai->filho_esquerdo)
+		{
+			arvore[posicao-1]->pai->filho_esquerdo = arvore[posicao-1]->filho_esquerdo;
+			if (arvore[posicao-1]->irmao_direito)
+			{
+				arvore[posicao-1]->pai->filho_esquerdo->addirmao(arvore[posicao-1]->irmao_direito);
+			}
+			arvore.erase(arvore.begin() + posicao-1); 
+		}else
+		{
+			no *temp = arvore[posicao-1]->pai->filho_esquerdo;
+			while (temp->irmao_direito != arvore[posicao-1])
+			{
+				temp = temp->irmao_direito;
+			}
+			temp->irmao_direito = arvore[posicao-1]->filho_esquerdo;
+
+			if (arvore[posicao-1]->irmao_direito)
+			{
+				temp->irmao_direito->addirmao(arvore[posicao-1]->irmao_direito);
+			}
+			arvore.erase(arvore.begin() + posicao-1); 
 		}
 	}
 
@@ -144,6 +265,22 @@ class Tree
 	}
 
 	bool busca_profundidade(const T chave)
+	{
+		for (int i = 0; i < int(arvore.size()); i++)
+		{
+			if (arvore[i]->pai == nullptr)
+			{
+				bool busca = arvore[i]->busca_profundidade(chave);
+				if (busca)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	bool busca_largura(const T chave)
 	{
 		for (int i = 0; i < int(arvore.size()); i++)
 		{
--- main.cpp
+++ main.cpp
@@ -9,14 +9,20 @@ int main()
 	arvore.add(4, 1);
 	arvore.add(2, 1);
 	arvore.add(1, 2);
-	arvore.add(6, 4);
 	arvore.add(9, 3);
+	arvore.add(5, 4);
+	arvore.add(6, 4);
 	arvore.print();
-	cout << "profundidade de 1: " << arvore.profundidade(4);
+	cout << endl;
+	cout << "profundidade de 1: " << arvore.profundidade(4) << endl;
 	cout << " altura arvore " << arvore.altura() << endl;
-	if (arvore.busca_profundidade(9))
+	if (arvore.busca_largura(5))
 	{
-		cout << "achou";
+		cout << "achou" << endl;
 	}
+	cout << "testando exclusão " << endl;
+
+	arvore.remove(3);
+	arvore.print();
 	return 0;
 }
